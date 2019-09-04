61785f14eafecccf5f6bf0e6643a5013a2f85dfc
Graham Fairweather <xotic750@gmail.com>
2019-09-05 01:54:48 +0200

    Maintenance

--- dist/assert-is-callable-x.js
+++ dist/assert-is-callable-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-30T22:30:23.532Z",
+  "date": "2019-09-04T23:54:42.055Z",
   "describe": "",
   "description": "If IsCallable(callbackfn) is false, throw a TypeError exception.",
   "file": "assert-is-callable-x.js",
